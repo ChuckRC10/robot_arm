948f2b130702ddbf7883e71603c178d298a00b32
ChuckRC10 <ccurione@purdue.edu>
2025-04-17 23:17:47 -0400

    testing

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 
-len1 = 200
-len2 = 200
+len1 = 100
+len2 = 100
 
 damping_const = 200
 max_pos_delta = 6
 
-screen_size = [1000, 1000]
+screen_size = [500, 500]
 
 line_color1 = (255, 0, 0)
 line_color2 = (0, 255, 0)
