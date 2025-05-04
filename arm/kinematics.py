71b5de2eca8f7226c4ea0dfaec4fcce55979ae7f
ChuckRC10 <113194872+ChuckRC10@users.noreply.github.com>
2025-05-04 17:21:39 -0500

    clean up and fix vocabulary for ellipse functions

--- arm/kinematics.py
+++ arm/kinematics.py
@@ -22,28 +22,25 @@ def getVelocityEllipseAngle(ellipseEquationMatrix):
 def get_ellipse_size(ellipseEquationMatrix):
     #TODO: Test if this even works right
     
-    eigenvalue_array = jnp.linalg.eigvals(ellipseEquationMatrix)
+    eigenvalues = jnp.real(jnp.linalg.eigvals(ellipseEquationMatrix))
 
-    new_A_coefficient = jnp.real(eigenvalue_array[0])
-    new_B_coefficient = jnp.real(eigenvalue_array[1])
-
-    major_axis_size = 1/jnp.sqrt(new_A_coefficient)
-    minor_axis_size = 1/jnp.sqrt(new_B_coefficient)
+    axisSize1 = 1/jnp.sqrt(eigenvalues[0])
+    axisSize2 = 1/jnp.sqrt(eigenvalues[1])
 
     maximum_size = 200
     minimum_size = .1
-    if jnp.isinf(major_axis_size):
-        major_axis_size = maximum_size
-    if jnp.isinf(minor_axis_size):
-        minor_axis_size = maximum_size
+    if jnp.isinf(axisSize1):
+        axisSize1 = maximum_size
+    if jnp.isinf(axisSize2):
+        axisSize2 = maximum_size
 
-    if jnp.isnan(major_axis_size) or major_axis_size < minimum_size:
-        major_axis_size = minimum_size
-    if jnp.isnan(minor_axis_size) or minor_axis_size < minimum_size:
-        minor_axis_size = minimum_size
+    if jnp.isnan(axisSize1) or axisSize1 < minimum_size:
+        axisSize1 = minimum_size
+    if jnp.isnan(axisSize2) or axisSize2 < minimum_size:
+        axisSize2 = minimum_size
 
-    return jnp.array([major_axis_size, minor_axis_size])
+    return jnp.array([axisSize1, axisSize2])
 
-def getEllipseArea(majorAxisSize, minorAxisSize) -> float:
-    area = jnp.pi * majorAxisSize * minorAxisSize
+def getEllipseArea(axisSize1, axisSize2) -> float:
+    area = jnp.pi * axisSize1 * axisSize2
     return float(area)
